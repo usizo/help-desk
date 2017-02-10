b732aa072d3ba9c5893a28177044abf4961d16c5
Admin <admin@Admins-MacBook-Pro.local>
2017-02-09 22:37:38 -0800

    router WORKS with node and express

---
+++ server/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.listen(3000);
+
+module.exports = app;
