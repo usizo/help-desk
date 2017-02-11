50e4e9a44f5a6c9be04bfe3b26137f51510bf6e1
Curt Toppel <ctoppel@aol.com>
2017-02-11 10:48:25 -0800

    complete

--- server/pgConnection.js
+++ server/pgConnection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('postgres://usizo:123456@localhost:5432/helpdesk');
+const connection = new Sequelize('postgres://sjewxpel:tynvapNbP-8ig-LVnhHQhdZSNf95o7kB@babar.elephantsql.com:5432/sjewxpel');
 
 module.exports = connection;
